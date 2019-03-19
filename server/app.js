5385a26ee683b8fb2c19618017cfea751f793d08
Gary Pang <gary.pang@gmail.com>
2019-03-19 11:41:14 -0400

    Load NYC Open Data API token as env variable

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 let port = process.env.PORT || 3000;
+const token = process.env.API_TOKEN || require('../config/token');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
