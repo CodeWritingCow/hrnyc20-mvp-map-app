2d07d0676ad88265002c353cf187ba2ea2427bb2
Gary Pang <gary.pang@gmail.com>
2019-03-19 14:56:43 -0400

    Create controller that makes GET requests to NYC API

--- public/map.js
+++ public/map.js
@@ -21,32 +21,40 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
 
 let nypdData;
 
-fetch('https://data.cityofnewyork.us/resource/5ucz-vwe8.json?statistical_murder_flag=true')
-    .then((res) => {
-        return res.json();
-    })
-    .then((myJson) => {
-        nypdData = JSON.stringify(myJson);
-        myJson.forEach((crime) => {
-            L.marker([crime.latitude, crime.longitude], {icon: skullMarker}) 
-                .addTo(nycMap)
-                .bindPopup(`
-                <h3>${crime.occur_date}</h3>
-                <hr/>
-                <h5>${crime.occur_time}</h5>
-                <h5>${crime.boro}</h5>
-                <p>${crime.location_desc || ''}</p>
-                <h5>Victim</h5>
-                <p>Sex: ${getGender(crime.vic_sex)}</p>
-                <p>Age: ${crime.vic_age_group}</p>
-                <p>Race: ${crime.vic_race}</p>
-                <h5>Suspect</h5>
-                <p>Sex: ${getGender(crime.perp_sex)}</p>
-                <p>Age: ${crime.perp_age_group || 'Unknown'}</p>
-                <p>Race: ${crime.perp_race || 'Unknown'}</p>                        
-                `);                    
-        });
-        document.getElementById('deathCounter').textContent = JSON.parse(nypdData).length;
+fetch('/api/homicides')
+    .then((response) => {
+        // console.log('response:', response.text());
+        response.text().then((data) => {
+            // console.log('data:', data);
+            // console.log(typeof data); -> string
+                        
+            JSON.parse(data).forEach((crime) => {
+                // console.log('crime:', crime);
+                
+                L.marker([crime.latitude, crime.longitude], {icon: skullMarker}) 
+                    .addTo(nycMap)
+                    .bindPopup(`
+                    <h3>${crime.occur_date}</h3>
+                    <hr/>
+                    <h5>${crime.occur_time}</h5>
+                    <h5>${crime.boro}</h5>
+                    <p>${crime.location_desc || ''}</p>
+                    <h5>Victim</h5>
+                    <p>Sex: ${getGender(crime.vic_sex)}</p>
+                    <p>Age: ${crime.vic_age_group}</p>
+                    <p>Race: ${crime.vic_race}</p>
+                    <h5>Suspect</h5>
+                    <p>Sex: ${getGender(crime.perp_sex)}</p>
+                    <p>Age: ${crime.perp_age_group || 'Unknown'}</p>
+                    <p>Race: ${crime.perp_race || 'Unknown'}</p>                        
+                    `);                    
+            });
+            document.getElementById('deathCounter').textContent = JSON.parse(data).length;
+        })
+        
+        
+}).catch((err) => {
+    console.log(err);
 });
 
 let getGender = function (gender) {
