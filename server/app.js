b974f897ec2504a4add425a158137614b6ad1c9f
Gary Pang <gary.pang@gmail.com>
2019-03-19 11:31:05 -0400

    Set up Node.js server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../index.html'));
+});
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
