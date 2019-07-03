1cee4660416ae57ef7fdea2418168cf84c27df26
Gary Pang <gary.pang@gmail.com>
2019-07-03 00:06:03 -0400

    Update getNypdData() to fetch data from historic API
    If a year is passed as an argument to getNypdData, it fetches data
    for that year from a historic-data API. Otherwise, getNypdData
    fetches data for the current calendar year from another API.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,12 @@
 const token = process.env.API_TOKEN || require('../../config/token');
 const axios = require('axios');
 
-const getNypdData = function () {
-    return axios.get(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?statistical_murder_flag=true&$$app_token=${token}`);
+const getNypdData = function (year) {
+    if (!year) {
+        return axios.get(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?statistical_murder_flag=true&$$app_token=${token}`);
+    } else {
+        return axios.get(`https://data.cityofnewyork.us/resource/833y-fsy8.json?statistical_murder_flag=true&$where=occur_date%20between%20%27${year}-01-01T00:00:00%27%20and%20%27${year}-12-31T00:00:00%27&$$app_token=${token}`);
+    }
 }
 
 module.exports = {
