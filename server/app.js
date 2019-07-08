396e4b07ab01204cd66af4f301ea734d818a49c0
Gary Pang <gary.pang@gmail.com>
2019-07-08 02:38:07 -0400

    Install NPM module response-time
    Add X-Response-Time header to Node.js server responses.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const responseTime = require('response-time');
 
 const controller = require('./controllers');
 let port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../public'));
 app.use(cors());
+app.use(responseTime());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/../index.html'));
