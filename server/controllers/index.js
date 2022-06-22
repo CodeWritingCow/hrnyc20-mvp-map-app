b238159f6f7529bedae4e0412bae7bbd1b2532b7
Gary Pang <gary.pang@gmail.com>
2022-06-22 01:11:44 -0400

    Fix bug where app doesn't load current-year data
    Change value of URL param "statistical_murder_flag" from "true" to "Y."
    See #22

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const getNypdData = function (year) {
     if (!year || year === Date().split(' ')[3]) {
-        return axios.get(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?statistical_murder_flag=true&$$app_token=${token}`);
+        return axios.get(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?statistical_murder_flag=Y&$$app_token=${token}`);
     } else {
         return axios.get(`https://data.cityofnewyork.us/resource/833y-fsy8.json?statistical_murder_flag=true&$where=occur_date%20between%20%27${year}-01-01T00:00:00%27%20and%20%27${year}-12-31T00:00:00%27&$$app_token=${token}`);
     }
